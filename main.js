d8b9855b300c0c3f7008a3318248ff1cd3c12f3d
Aidar <aidar.lutfullin.1992@gmail.com>
2021-11-12 13:32:18 +0300

    commit 4

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 let game = {
   ctx: null,
+  platform: null,
+  ball: null,
+  blocks: [],
+  rows: 4,
+  cols: 6,
 
   sprites: {
     background: null,
@@ -16,15 +21,28 @@ let game = {
     let loaded = 0;
     let required = Object.keys(this.sprites).length;
 
+    let onImageLoad = () => {
+      ++loaded;
+      if (loaded >= required) {
+        callback();
+      }
+    };
+
     for (let key in this.sprites) {
       this.sprites[key] = new Image();
       this.sprites[key].src = "img/" + key + ".png";
-      this.sprites[key].addEventListener("load", () => {
-        ++loaded;
-        if (loaded >= required) {
-          callback();
-        }
-      });
+      this.sprites[key].addEventListener("load", onImageLoad);
+    }
+  },
+
+  create() {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
+        this.blocks.push({
+          x: 85 * col + 65,
+          y: 34 * row + 35,
+        });
+      }
     }
   },
 
@@ -38,24 +56,32 @@ let game = {
     this.ctx.drawImage(this.sprites.background, 0, 0);
     this.ctx.drawImage(this.sprites.ball, this.ball.x, this.ball.y);
     this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
-    this.ctx.drawImage(this.sprites.block, 250, 50);
+
+    this.renderBlocks();
+  },
+
+  renderBlocks() {
+    for (let block of this.blocks) {
+      this.ctx.drawImage(this.sprites.block, block.x, block.y);
+    }
   },
 
   start: function () {
     this.init();
     this.preload(() => {
+      this.create();
       this.run();
     });
   },
 };
 
 game.ball = {
-  x: 290,
+  x: 300,
   y: 310,
 };
 
 game.platform = {
-  x: 250,
+  x: 270,
   y: 340,
 };
 
