1b4ef1e866382c2eb54e1e4f401ffbc44759983a
Aidar <aidar.lutfullin.1992@gmail.com>
2021-11-30 12:15:24 +0300

    commit 13

--- main.js
+++ main.js
@@ -59,6 +59,7 @@ let game = {
     for (let row = 0; row < this.rows; row++) {
       for (let col = 0; col < this.cols; col++) {
         this.blocks.push({
+          active: true,
           width: 80,
           height: 29,
           x: 85 * col + 65,
@@ -77,7 +78,7 @@ let game = {
 
   collideBlocks() {
     for (let block of this.blocks) {
-      if (this.ball.collide(block)) {
+      if (block.active && this.ball.collide(block)) {
         this.ball.bumpBlock(block);
       }
     }
@@ -107,7 +108,9 @@ let game = {
 
   renderBlocks() {
     for (let block of this.blocks) {
-      this.ctx.drawImage(this.sprites.block, block.x, block.y);
+      if (block.active) {
+        this.ctx.drawImage(this.sprites.block, block.x, block.y);
+      }
     }
   },
 
@@ -163,6 +166,7 @@ game.ball = {
 
   bumpBlock(block) {
     this.dy *= -1;
+    block.active = false;
   },
 
   bumpPlatform(platform) {
